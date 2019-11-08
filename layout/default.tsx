e11b55358dc31b20fe70040c547bed9b06294fb4
h.kunimatsu <h.kunimatsu@i-tap.co.jp>
2019-11-08 15:10:44 +0900

    [feat] next.js を 9.1.3 にアップデート

---
+++ layout/default.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// next
+import { NextComponentType } from 'next';
+import Head from 'next/head';
+
+// components
+import Header from '~/components/Header';
+import Container from '~/components/Container';
+
+const Home: NextComponentType = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+      <link rel="icon" href="/favicon.ico" />
+      <link href="/global.css" rel="stylesheet" />
+    </Head>
+
+    <Header />
+    <Container>
+      <Title>Next.js! boilerplate</Title>
+      <p>&nbsp;</p>
+    </Container>
+  </div>
+);
+
+const Title = styled.h1`
+  font-size: 24px;
+  color: #334;
+`;
+
+export default Home;
